a08fe15b3ab69ba163efb7e0eeac6723e2d937ef
Ben Kaplan <benjamin.kaplan@case.edu>
2011-01-30 01:10:27 -0500

    This isn't the actual final code, it's slightly modified.

--- bsk16_hw2/src/command_publisher.cpp
+++ bsk16_hw2/src/command_publisher.cpp
@@ -1,5 +1,48 @@
 #include<ros/ros.h>
 #include<geometry_msgs/Twist.h> //data type for velocities
+#include<math.h>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+const double MAX_SPEED = 2.0;
+const double MAX_ROTATE = 2.0;
+const double MAX_ACCEL = 2.0;
+const double REFRESH_RATE = 0.1;
+const double MAX_ANGLE_ACCEL = 1.0;
+double getRobotVelocity(double cur_vel, double distance_to_dest) {
+  double distance_if_decel = 0.5 * cur_vel * (cur_vel / (MAX_ACCEL * REFRESH_RATE));
+  if(distance_to_dest - distance_if_decel < 0.2) {
+    return max(cur_vel - MAC_ACCEL * REFRESH_RATE, min(0.2,distance_to_dest));
+  } else if(cur_vel < MAX_SPEED) {
+    return cur_vel + MAX_ACCEL * REFRESH_RATE;
+  } else {
+    return cur_vel;
+  }
+}
+
+double goDistance(double *velocity, double distance, double time_period) {
+  *velocity = getRobotVelocity(*velocity, distance);
+  double distance_returned = distance - *velocity * time_period;
+  return distance_returned;
+}
+
+double getRobotRotation(double cur_rotate, double remaining_rotate) {
+  double rotate_if_decel = -0.5 * cur_rotate * (cur_rotate / (REFRESH_RATE * MAX_ANGLE_ACCEL));
+  if(rotate_if_decel - remaining_rotate < -0.1) {
+    return min(cur_rotate + (REFRESH_RATE * MAX_ANGLE_ACCEL), max(remaining_rotate, -0.1));
+  } else if(fabs(cur_rotate) < MAX_ROTATE) {
+    return cur_rotate - MAX_ANGLE_ACCEL * REFRESH_RATE;
+  } else {
+    return cur_rotate;
+  }
+}
+
+double goRotate(double *rotation, double rotate, double time_period) {
+  *rotation = getRobotRotation(*rotation, rotate);
+   double rotate_returned = rotate  - *rotation * time_period;
+  return rotate_returned;
+
+}
 int main(int argc,char **argv)
 {
 	ros::init(argc,argv,"command_publisher");//name of this node
@@ -10,21 +53,37 @@ int main(int argc,char **argv)
 
 	//send robot forward for 3 seconds, reiterated at 10Hz.  Need a ROS "rate" object to enforce a rate
 	geometry_msgs::Twist vel_object;
-	ros::Duration run_duration(3.0); // specify desired duration of this command segment to be 3 seconds
+	ros::Duration run_duration(22.0); // specify desired duration of this command segment to be 3 seconds
 	ros::Duration elapsed_time; // define a variable to hold elapsed time
 	ros::Rate naptime(10); //will perform sleeps to enforce loop rate of "10" Hz
 	while (!ros::Time::isValid()) {} // simulation time sometimes initializes slowly. Wait until ros::Time::now() will be valid
 	ros::Time birthday= ros::Time::now(); // get the current time, which defines our start time, called "birthday"
 	ROS_INFO("birthday started as %f", birthday.toSec());
+  int stage = 0;
+  double amounts_to_change[] = {3.0,asin(-1),12.2,asin(-1),4,-1};
+
+  double amount_to_change = 0.0;
 	while (ros::ok()) // do work here
 	{
 		elapsed_time= ros::Time::now()-birthday;
 		ROS_INFO("birthday is %f", birthday.toSec());
 		ROS_INFO("elapsed time is %f", elapsed_time.toSec());
-		vel_object.linear.x = 1.0; // command a forward velocity of 1m/sec
-		vel_object.angular.z = 0.0; //command a spin rate of 0.0 rad/sec
-		if ( elapsed_time < run_duration)
-		{
+    cout << amount_to_change <<endl;
+    if(fabs(amount_to_change) <=  0.02) {
+      cout << "Increasing Stage";
+      amount_to_change = amounts_to_change[stage];
+      stage++;
+      vel_object.linear.x = 0.0;
+      vel_object.angular.z = 0.0;
+    }
+    if(stage == 1 || stage == 3  || stage == 5) {
+      amount_to_change = goDistance(&(vel_object.linear.x),amount_to_change,0.1);
+       
+    } else if(stage == 2 || stage == 4) {
+      amount_to_change = goRotate(&(vel_object.angular.z),amount_to_change, 0.1);
+		}
+
+    if(stage < 6) {
 			// send out new command appropriate for this instant;
 			// boring--always send out the same speed/spin values in this example
 			pub.publish(vel_object);  // this action causes the commands in vel_object to be published 
