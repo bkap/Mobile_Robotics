b835a16367b846ed3d4f02652c437cbf4c8ee76e
wesley gould <wes@1337-m4ch1n3.(none)>
2011-04-23 21:08:00 -0400

    adder PlannerFuncs

---
+++ delta/src/PlannerFuncs.h
@@ -0,0 +1,40 @@
+
+#include "command_publisher.h"
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <tf/transform_datatypes.h>
+#include <tf/transform_listener.h>
+#include <image_transport/image_transport.h>
+#include <opencv/cv.h>
+#include <opencv2/core/core.hpp>
+#include <opencv/highgui.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/core/mat.hpp>
+#include <list>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+#include <iostream>
+#include <eecs376_msgs/PathSegment.h>
+#include <eecs376_msgs/PathList.h>
+#include <eecs376_msgs/CrawlerDesiredState.h>
+#include <visualization_msgs/Marker.h>
+#include "CSpaceFuncs.h"
+
+using namespace cv;
+using namespace std;
+using namespace eecs376_msgs;
+
+vector<Point2i> aStar (Mat map, Point2i start);
+vector<Point2f> convertToMap(vector<Point2i> victor);
+vector<point2f> reducePoints(vector<Point2f> victor);
+
+class Node
+{
+	int x, y;
+	Node* Parent;
+	int heuristic;
+	int pathCost;
+}
