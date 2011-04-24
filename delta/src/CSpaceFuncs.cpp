0853a1913e055d79fbef9c9755ad544e0f2b701e
Bill Kulp <bk8190@gmail.com>
2011-04-23 20:38:21 -0400

    Added a function to convert an eecs376_msgs::PathList into a visualization_msgs::MarkerArray for viewing in rviz

--- delta/src/CSpaceFuncs.cpp
+++ delta/src/CSpaceFuncs.cpp
@@ -12,6 +12,9 @@
 #include <algorithm>
 #include <iostream>
 #include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <eecs376_msgs/PathSegment.h>
+#include <eecs376_msgs/PathList.h>
 #include <vector>
 #ifndef CSPACEFUNCS_cpp
 #define CSPACEFUNCS_cpp
@@ -35,6 +38,52 @@ Mat_<bool>* getMap(const nav_msgs::OccupancyGrid& grid) {
 
 }
 
+// Creates an array of markers for visualization in RVIZ from a vector of points
+visualization_msgs::MarkerArray visualizePoints(eecs376_msgs::PathList plan)
+{
+	visualization_msgs::MarkerArray markers;
+	visualization_msgs::Marker *marker;
+	
+	// Create a marker for each point
+	for( int i=0; i<plan.path_list.size(); i++ )
+	{
+		// Create a new marker
+		marker = new visualization_msgs::Marker();
+		
+		// Fill out the header
+		marker->header.frame_id = "odom";
+		marker->header.stamp = ros::Time();
+		marker->ns = "my_namespace";
+		marker->id = plan.path_list[i].seg_number;
+		marker->type = visualization_msgs::Marker::LINE_STRIP;
+		marker->action = visualization_msgs::Marker::ADD;
+		
+		// Copy the ref point from the path segment
+		marker->pose.position.x = plan.path_list[i].ref_point.x;
+		marker->pose.position.y = plan.path_list[i].ref_point.y;
+		marker->pose.position.z = plan.path_list[i].ref_point.z;
+		
+		marker->pose.orientation.x = 0.0;
+		marker->pose.orientation.y = 0.0;
+		marker->pose.orientation.z = 0.0;
+		marker->pose.orientation.w = 1.0;
+		
+		// Line is .1m thick (reduce this once it is working)
+		marker->scale.x = 0.1;
+		marker->scale.y = 0.1;
+		marker->scale.z = 0.1;
+		
+		marker->color.a = 1.0;
+		
+		marker->color.r = 0;
+		marker->color.g = 255;
+		marker->color.b = 0;
+		
+		markers.markers.push_back(*marker);
+	}
+	
+	return(markers);
+}
 
 //plotmap should try to plot visualization messages to rviz so they can be seen.  This is just a generic wrapper around the boilerplate so that we could visualize pathlists and such with fewer calls.  TODO: make the publisher static
 void PlotMap(list<geometry_msgs::Point> PointsToPlot, ros::Publisher *vis_pub, float r, float g, float b, float cubesize)
@@ -72,7 +121,6 @@ void PlotMap(list<geometry_msgs::Point> PointsToPlot, ros::Publisher *vis_pub, f
 		geometry_msgs::Point P = PointsToPlot.back();
 		PointsToPlot.pop_back();
 		RetVal.push_back(P);
-		
 	}
 	marker.points = RetVal;
 	//marker.colors = NULL;  //the wiki says that this should 
